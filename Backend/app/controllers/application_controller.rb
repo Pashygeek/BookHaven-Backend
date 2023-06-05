da23246d9acd2c00389b651abf69d52857eaf834
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-06-06 00:04:39 +0300

    Changed port  number and added a bakend folder

---
+++ Backend/app/controllers/application_controller.rb
@@ -0,0 +1,90 @@
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  
+  # Add your routes here
+  get "/" do
+    { message: "Welcome To BookHaven!" }.to_json
+  end
+
+  get '/books' do
+    books = Book.all
+    books.to_json
+  end
+
+  get '/books/:id' do
+    book = Book.find(params[:id])
+    book.to_json
+  end
+
+  post '/books' do
+    book = Book.create(title: params[:title], author: params[:author], description: params[:description], category_id: params[:category_id])
+    book.to_json
+  end
+
+  put '/books/:id' do
+    book = Book.find(params[:id])
+    book.update(title: params[:title], author: params[:author], description: params[:description], category_id: params[:category_id])
+    book.to_json
+  end
+
+  delete '/books/:id' do |id|
+    book = Book.find(id)
+    book.destroy
+    { message: 'Book deleted' }.to_json
+  end
+
+  put '/books/:id/rate' do |id|
+    book = Book.find(id)
+    rating = params[:rating].to_i
+    if rating >=1 && rating <= 5
+    book.update(rating: rating)
+    book.to_json
+  else
+    { error: 'Invalid rating' }.to_json
+  end
+end
+
+put '/books/:id/favorite' do |id|
+  book = Book.find(id)
+  book.update(favorite: true)
+  book.to_json
+end
+
+put '/book/:id/unfavorite' do |id|
+  book = Book.find(id)
+  book.update(favorite: false)
+  book.to_json
+end
+
+get '/categories' do
+  categories = Category.all
+  categories.to_json(include: :books)
+end
+
+get '/categories/:name' do
+  category = Category.find_by(name: params[:name])
+  if category
+    category_with_books = category.as_json(include: :books)
+    category_with_books.to_json
+  else
+    { error: 'Category not found' }.to_json
+  end
+end
+
+post '/categories' do
+  category = Category.create(name: params[:name])
+  category.to_json
+end
+
+put '/categories/:id' do |id|
+  category = Category.find(id)
+  category.update(name: params[:name])
+  category.to_json
+end
+
+delete '/categories/:id' do |id|
+  category = Category.find(id)
+  category.destroy
+  { message: 'Category deleted' }.to_json
+  end
+end
